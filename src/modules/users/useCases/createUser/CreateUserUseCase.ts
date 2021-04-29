75dd9ef6b373503e53b83b0b9947c405fb16c0d7
luiscostalafc <luiscostalafc@gmail.com>
2021-04-28 22:59:43 -0300

    feat: finished exam

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -13,7 +13,7 @@ class CreateUserUseCase {
     const userAlreadyExists = this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      throw new Error("user already exist!");
+      throw new Error("User already exist!");
     }
 
     return this.usersRepository.create({ name, email });
